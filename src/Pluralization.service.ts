602588ca5ae48d9eadfff7fc2b580184198ac51f
George Ionov <ionov@mdcm.co>
2020-04-25 20:37:36 +0400

    rename SmartPluralization to Pluralization

--- src/Pluralization.service.ts
+++ src/Pluralization.service.ts
@@ -1,30 +1,30 @@
 import StringHelper from './String.helper';
 import Locale from './Locale';
 
-export default class SmartPluralizationService {
-  private static instance: SmartPluralizationService | undefined;
+export default class PluralizationService {
+  private static instance: PluralizationService | undefined;
 
-  public static make(currentLocale: string, locales: { [p: string]: Locale} = {}): SmartPluralizationService {
+  public static make(currentLocale: string, locales: { [p: string]: Locale} = {}): PluralizationService {
     if (this.instance === undefined) {
       this.instance = new this(currentLocale, locales);
     }
     return this.instance;
   }
 
-  protected constructor(public currentLocale: string, public locales: { [index: string]: Locale } = {}) {
+  public constructor(public currentLocale: string, public locales: { [index: string]: Locale } = {}) {
   }
 
   public pluralize = (word: string, value: number, ...params: unknown[]): string => {
     return this.locales[this.currentLocale].pluralize(word, value, params);
   };
 
-  public pluralizeTemplate = (template: string, value: number, locale: string = this.currentLocale): string => {
+  public pluralizeTemplate = (template: string, value: number): string => {
     const placeholders = StringHelper.getPlaceholders(template, (placeholder: string) => {
       if (placeholder === 'n') {
         return '' + value;
       }
       const parts = placeholder.split('|');
-      return this.locales[locale].pluralize(parts[0], value, ...parts.slice(1));
+      return this.locales[this.currentLocale].pluralize(parts[0], value, ...parts.slice(1));
     });
     return StringHelper.replacePlaceholders(template, placeholders);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export * from './SmartPluralization.service';
+export * from './Pluralization.service';
 export * from './locales';
